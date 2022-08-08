c92862393504238e702c48fff7c15d0f650fdcd5
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2022-08-08 19:01:55 -0300

    implements PUT /movies/:id

--- main.go
+++ main.go
@@ -58,6 +58,22 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 }
 
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var movie Movie
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movie)
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -68,6 +84,7 @@ func main() {
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 
 	fmt.Printf("Starting server at port 8080\n")
 	http.ListenAndServe(":8080", r)
