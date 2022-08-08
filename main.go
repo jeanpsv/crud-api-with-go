a99e7e06b6e47c4fc8daf7670b12359be1948150
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2022-08-08 18:25:43 -0300

    implements GET /movies

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
@@ -10,4 +18,23 @@ type Movie struct {
 type Director struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "tt0816692", Title: "Interstellar", Director: &Director{Firstname: "Christopher", Lastname: "Nolan"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "tt0108778", Title: "Friends", Director: &Director{Firstname: "David", Lastname: "Crane"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+
+	fmt.Printf("Starting server at port 8080\n")
+	http.ListenAndServe(":8080", r)
 }
